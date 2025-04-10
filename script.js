a2fc18c09d4a98c29d19283b70d5861018843b56
Sergey Skokov <sergskokow@gmail.com>
2025-04-10 11:19:30 +0300

    Initial commit. Preparing for work.

---
+++ script.js
@@ -0,0 +1,178 @@
+State.variables.localization_ru = () => {
+    /* default */
+    l10nStrings.textAbort = 'Прервать';
+    l10nStrings.textAborting = 'Прерывание';
+    l10nStrings.textCancel = 'Отмена';
+    l10nStrings.textClear = 'Очистить';
+    l10nStrings.textClose = 'Закрыть';
+    l10nStrings.textDelete = 'Удалить';
+    l10nStrings.textExport = 'Экспорт';
+    l10nStrings.textIdentity = 'игра';
+    l10nStrings.textImport = 'Импорт';
+    l10nStrings.textLoad = 'Загрузить';
+    l10nStrings.textOff = 'Выкл';
+    l10nStrings.textOk = 'ОК';
+    l10nStrings.textOn = 'Вкл';
+    l10nStrings.textSave = 'Сохранить';
+    l10nStrings.textTurn = 'Ход';
+    l10nStrings.errorNonexistentPassage = 'фрагмент "{passage}" не существует';
+    l10nStrings.warningNoStorage = 'Отсутствуют все доступные API хранилища. Возможные причины: отключенная настройка сторонних файлов cookie, которая также влияет на веб-хранилище, или режим приватного просмотра.';
+    l10nStrings.warningNoWebStorage = 'API веб-хранилища отсутствует, поэтому этот {textIdentity} работает в режиме пониженной производительности. Вы можете продолжить, однако некоторые части могут работать некорректно.';
+    l10nStrings.warningDegraded = 'Некоторые возможности, необходимые для поддержки этого {textIdentity}, отсутствуют, поэтому он работает в режиме пониженной производительности. Вы можете продолжить, однако некоторые части могут работать неправильно.';
+    l10nStrings.warningNoSaves = 'Некоторые возможности, необходимые для поддержки сохранений, отсутствуют, поэтому сохранения были отключены для этого сеанса.';
+    l10nStrings.saveErrorDisallowed = 'Сохранение в настоящее время запрещено.';
+    l10nStrings.saveErrorDecodeFail = 'невозможно декодировать сохранение, вероятно, из-за повреждения';
+    l10nStrings.saveErrorDiskLoadFail = 'не удалось загрузить файл сохранения с диска';
+    l10nStrings.saveErrorIdMismatch = 'сохранение из неправильного {textIdentity}';
+    l10nStrings.saveErrorInvalidData = 'save отсутствуют требуемые данные, вероятно, из-за повреждения';
+    l10nStrings.saveErrorNonexistent = 'save не существует';
+    l10nStrings.uiBarLabelToggle = 'Переключить панель пользовательского интерфейса';
+    l10nStrings.uiBarLabelBackward = 'Перейти назад в истории {textIdentity}';
+    l10nStrings.uiBarLabelForward = 'Перейти вперед в истории {textIdentity}';
+    l10nStrings.uiBarLabelJumpto = 'Перейти к определенной точке в истории {textIdentity}';
+    l10nStrings.alertTitle = 'Оповещение';
+    l10nStrings.restartTitle = 'Перезапуск';
+    l10nStrings.restartMesgPrompt = 'Весь несохраненный прогресс будет утерян. Вы уверены, что хотите перезапустить?';
+    l10nStrings.continueTitle = 'Продолжить';
+    l10nStrings.savesTitle = 'Сохранения';
+    l10nStrings.savesHeaderBrowser = 'В браузере';
+    l10nStrings.savesHeaderDisk = 'На диске';
+    l10nStrings.savesLabelBrowserClear = 'Очистить все сохранения браузера';
+    l10nStrings.savesLabelBrowserExport = 'Экспортировать сохранения браузера в пакет';
+    l10nStrings.savesLabelBrowserImport = 'Импортировать сохранения браузера из пакета';
+    l10nStrings.savesLabelDiskLoad = 'Загрузить с диска';
+    l10nStrings.savesLabelDiskSave = 'Сохранить на диск';
+    l10nStrings.savesTextBrowserAuto = 'Авто';
+    l10nStrings.savesTextBrowserSlot = 'Слот';
+    l10nStrings.savesTextNoDate = 'неизвестная дата';
+    l10nStrings.settingsTitle = 'Настройки';
+    l10nStrings.settingsTextReset = 'Сброс к значениям по умолчанию';
+    l10nStrings.errorViewTitle = 'Ошибка';
+    l10nStrings.errorViewLabelToggle = 'Переключить вид ошибок';
+    l10nStrings.debugBarLabelToggle = 'Переключить панель отладки';
+    l10nStrings.debugBarLabelViewsToggle = 'Переключить представления отладки';
+    l10nStrings.debugBarLabelWatchAdd = 'Добавить новое наблюдение';
+    l10nStrings.debugBarLabelWatchAll = 'Следить за всеми';
+    l10nStrings.debugBarLabelWatchClear = 'Очистить все наблюдения';
+    l10nStrings.debugBarLabelWatchDelete = 'Удалить это наблюдение';
+    l10nStrings.debugBarLabelWatchPlaceholder = 'имя переменной';
+    l10nStrings.debugBarLabelPassagePlaceholder = 'имя фрагмента';
+    l10nStrings.debugBarLabelPassagePlay = 'Воспроизвести фрагмент';
+    l10nStrings.debugBarLabelWatchToggle = 'Переключить панель наблюдения';
+    l10nStrings.debugBarMesgNoWatches = 'Нет установленных наблюдений';
+    l10nStrings.debugBarTextAdd = 'Добавить';
+    l10nStrings.debugBarTextPassage = 'Фрагмент';
+    l10nStrings.debugBarTextViews = 'Просмотры';
+    l10nStrings.debugBarTextWatch = 'Наблюдение';
+    l10nStrings.macroBackText = 'Назад';
+    l10nStrings.macroReturnText = 'Возврат';
+    l10nStrings.autoloadTitle = 'Автозагрузка';
+    l10nStrings.autoloadMesgPrompt = 'Существует автосохранение. Загрузить сейчас или перейти к началу?';
+    l10nStrings.autoloadTextCancel = 'Перейти к началу';
+    l10nStrings.autoloadTextOk = 'Загрузить автосохранение';
+    l10nStrings.jumptoTitle = 'Перейти к';
+    l10nStrings.jumptoMesgUnavailable = 'В настоящее время нет доступных точек перехода\u2026';
+    l10nStrings.shareTitle = 'Поделиться';
+    /* custom */
+    l10nStrings.mechanica = new Object();
+};
+
+State.variables.localization_en = () => {
+    /* default */
+    l10nStrings.textAbort = 'Abort';
+    l10nStrings.textAborting = 'Aborting';
+    l10nStrings.textCancel = 'Cancel';
+    l10nStrings.textClear = 'Clear';
+    l10nStrings.textClose = 'Close';
+    l10nStrings.textDelete = 'Delete';
+    l10nStrings.textExport = 'Export';
+    l10nStrings.textIdentity = 'game';
+    l10nStrings.textImport = 'Import';
+    l10nStrings.textLoad = 'Load';
+    l10nStrings.textOff = 'Off';
+    l10nStrings.textOk = 'OK';
+    l10nStrings.textOn = 'On';
+    l10nStrings.textSave = 'Save';
+    l10nStrings.textTurn = 'Turn';
+    l10nStrings.errorNonexistentPassage = 'the passage "{passage}" does not exist';
+    l10nStrings.warningNoStorage = 'All usable storage APIs are missing. Possible causes are a disabled third-party cookie setting, which also affects Web Storage, or a private browsing mode.';
+    l10nStrings.warningNoWebStorage = 'The Web Storage API is missing, so this {textIdentity} is running in a degraded mode. You may be able to continue, however, some parts may not work properly.';
+    l10nStrings.warningDegraded = 'Some capabilities required to support this {textIdentity} are missing, so it is running in a degraded mode. You may be able to continue, however, some parts may not work properly.';
+    l10nStrings.warningNoSaves = 'Some capabilities required to support saves are missing, so saves have been disabled for this session.';
+    l10nStrings.saveErrorDisallowed = 'Saving is currently disallowed.';
+    l10nStrings.saveErrorDecodeFail = 'unable to decode save, likely due to corruption';
+    l10nStrings.saveErrorDiskLoadFail = 'failed to load save file from disk';
+    l10nStrings.saveErrorIdMismatch = 'save is from the wrong {textIdentity}';
+    l10nStrings.saveErrorInvalidData = 'save is missing required data, likely due to corruption';
+    l10nStrings.saveErrorNonexistent = 'save does not exist';
+    l10nStrings.uiBarLabelToggle = 'Toggle the UI bar';
+    l10nStrings.uiBarLabelBackward = 'Go backward within the {textIdentity} history';
+    l10nStrings.uiBarLabelForward = 'Go forward within the {textIdentity} history';
+    l10nStrings.uiBarLabelJumpto = 'Jump to a specific point within the {textIdentity} history';
+    l10nStrings.alertTitle = 'Alert';
+    l10nStrings.restartTitle = 'Restart';
+    l10nStrings.restartMesgPrompt = 'All unsaved progress will be lost. Are you sure that you want to restart?';
+    l10nStrings.continueTitle = 'Continue';
+    l10nStrings.savesTitle = 'Saves';
+    l10nStrings.savesHeaderBrowser = 'In Browser';
+    l10nStrings.savesHeaderDisk = 'On Disk';
+    l10nStrings.savesLabelBrowserClear = 'Clear all browser saves';
+    l10nStrings.savesLabelBrowserExport = 'Export browser saves to bundle';
+    l10nStrings.savesLabelBrowserImport = 'Import browser saves from bundle';
+    l10nStrings.savesLabelDiskLoad = 'Load from disk';
+    l10nStrings.savesLabelDiskSave = 'Save to disk';
+    l10nStrings.savesTextBrowserAuto = 'Auto';
+    l10nStrings.savesTextBrowserSlot = 'Slot';
+    l10nStrings.savesTextNoDate = 'unknown date';
+    l10nStrings.settingsTitle = 'Settings';
+    l10nStrings.settingsTextReset = 'Reset to Defaults';
+    l10nStrings.errorViewTitle = 'Error';
+    l10nStrings.errorViewLabelToggle = 'Toggle the error view';
+    l10nStrings.debugBarLabelToggle = 'Toggle the debug bar';
+    l10nStrings.debugBarLabelViewsToggle = 'Toggle the debug views';
+    l10nStrings.debugBarLabelWatchAdd = 'Add a new watch';
+    l10nStrings.debugBarLabelWatchAll = 'Watch all';
+    l10nStrings.debugBarLabelWatchClear = 'Clear all watches';
+    l10nStrings.debugBarLabelWatchDelete = 'Delete this watch';
+    l10nStrings.debugBarLabelWatchPlaceholder = 'variable name';
+    l10nStrings.debugBarLabelPassagePlaceholder = 'passage name';
+    l10nStrings.debugBarLabelPassagePlay = 'Play passage';
+    l10nStrings.debugBarLabelWatchToggle = 'Toggle the watch panel';
+    l10nStrings.debugBarMesgNoWatches = 'No watches set';
+    l10nStrings.debugBarTextAdd = 'Add';
+    l10nStrings.debugBarTextPassage = 'Passage';
+    l10nStrings.debugBarTextViews = 'Views';
+    l10nStrings.debugBarTextWatch = 'Watch';
+    l10nStrings.macroBackText = 'Back';
+    l10nStrings.macroReturnText = 'Return';
+    l10nStrings.autoloadTitle = 'Autoload';
+    l10nStrings.autoloadMesgPrompt = 'An autosave exists. Load it now or go to the start?';
+    l10nStrings.autoloadTextCancel = 'Go to start';
+    l10nStrings.autoloadTextOk = 'Load autosave';
+    l10nStrings.jumptoTitle = 'Jump To';
+    l10nStrings.jumptoMesgUnavailable = 'No jump points currently available\u2026';
+    l10nStrings.shareTitle = 'Share';
+    /* custom */
+    l10nStrings.mechanica = new Object();
+};
+
+let languageChanged = () => {
+    switch (settings.language) {
+        case "🇷🇺 Русский":
+            State.variables.localization_ru();
+            break;
+        default:
+            State.variables.localization_en();
+            break;
+    }
+};
+
+languageChanged();
+
+Setting.addList("language", {
+    label: "Language",
+    list: ["🇷🇺 Русский", "🇺🇸 English"], 
+    default: "🇷🇺 Русский",
+    onInit: languageChanged,
+    onChange: languageChanged
+});
